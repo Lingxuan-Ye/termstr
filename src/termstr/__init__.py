9147ab1bcd24fd8b97fd5d25fd4054039363fbed
Lingxuan Ye <love@lingxuan.io>
2023-11-19 23:30:59 +0800

    Add convenient function `cprint`

--- src/termstr/__init__.py
+++ src/termstr/__init__.py
@@ -1,12 +1,13 @@
 from .const import ESCSEQ, Color
 from .models import Div, Span
-from .utils import erase_screen, reset_cursor, tstr
+from .utils import cprint, erase_screen, reset_cursor, tstr
 
 __all__ = [
     "ESCSEQ",
     "Color",
     "Div",
     "Span",
+    "cprint",
     "erase_screen",
     "reset_cursor",
     "tstr",
--- src/termstr/utils.py
+++ src/termstr/utils.py
@@ -1,5 +1,21 @@
-from .const import ESCSEQ
+import sys
+
+from .const import ESCSEQ, Color
 from .models import Span
+
+
+def cprint(
+    label: str,
+    message: str,
+    *,
+    label_color: Color,
+    label_width: int = 8,
+    to_stderr: bool = False,
+) -> None:
+    clabel = Span(label).set_bold().set_foreground(label_color).rjust(label_width)
+    if to_stderr:
+        return print(clabel, message, file=sys.stderr)
+    print(clabel, message, file=sys.stdout)
 
 
 def erase_screen() -> None:
